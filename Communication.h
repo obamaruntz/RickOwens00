76d6b216cdb3bde65f5fadad53296859c627ecc4
void* nullptr <78532797+voidnullptr@users.noreply.github.com>
2025-06-16 22:55:08 +0100

    Update Communication.h

--- Communication.h
+++ Communication.h
@@ -3,8 +3,9 @@
 #include <tlhelp32.h>
 #include <cstdint>
 #include <string>
+#include <vector>
 
-#include <framework.h>
+#include "Definitions.h"
 
 #define PRW_CODE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2ec33, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
 #define VRW_ATTACH_CODE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2ec34, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
@@ -12,71 +13,41 @@
 #define BA_CODE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2ec36, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)  
 #define SECURITY_CODE 0x94c9e4bc3
 
-struct _PRW {
-	u64 security_code;
-
-	i32 process_id;
-	void* address;
-	void* buffer;
-
-	u64 size;
-	u64 return_size;
-
-	bool Type;
-};
-
-struct _VRW {
-	u64 security_code;
-
-	HANDLE process_handle;
-	void* address;
-	void* buffer;
-
-	u64 size;
-	u64 return_size;
-
-	bool Type;
-};
-
-struct _BA {
-	u64 security_code;
-
-	i32 process_id;
-	u64* address;
-};
-
-class communication {
+class Communication {
 public:
-	communication();
-	~communication();
+	Communication();
+	~Communication();
 
-	bool is_connected();
+	bool isConnected();
 
-	bool v_attach(i32 process_id);
+	bool virtualAttach(i32 processId);
 
-	i32 find_process(const i8* ProcessName); // UM
-	u64 find_image();
+	i32 findProcess(const i8* processName); // UM
+	u64 findImage();
 
 	template <typename T>
-	T v_read(u64 address);
+	T virtualRead(u64 address);
 	template <typename T>
-	void v_write(u64 address, T& Value);
+	void virtualWrite(u64 address, T& value);
 
 	template <typename T>
 	T read(u64 address);
 	template <typename T>
-	void write(u64 address, T& Value);
+	void write(u64 address, T& value);
 
-	std::string readstr(u64 address);
+	std::string readString(u64 address);
 
-	u64 image_address = 0;
-	i32 process_id = 0;
+	HANDLE getDriverHandle();
+	i32 getProcessId();
+	u64 getImageAddress();
 private:
-	HANDLE driver_handle = INVALID_HANDLE_VALUE;
+	HANDLE driverHandle = INVALID_HANDLE_VALUE;
+	i32 processId = 0;
+	u64 imageAddress = 0;
 };
 
 template <typename T>
-T communication::v_read(u64 address) {
+T Communication::virtualRead(u64 address) {
 	T temp = {};
 
 	_VRW arguments;
@@ -86,12 +57,12 @@ T communication::v_read(u64 address) {
 	arguments.size = sizeof(T);
 	arguments.Type = false;
 
-	DeviceIoControl(driver_handle, VRW_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
+	DeviceIoControl(driverHandle, VRW_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
 	return temp;
 }
 
 template <typename T>
-void communication::v_write(u64 address, T& value) {
+void Communication::virtualWrite(u64 address, T& value) {
 	_VRW arguments;
 	arguments.security_code = SECURITY_CODE;
 	arguments.address = reinterpret_cast<void*>(address);
@@ -99,11 +70,11 @@ void communication::v_write(u64 address, T& value) {
 	arguments.size = sizeof(T);
 	arguments.Type = true;
 
-	DeviceIoControl(driver_handle, VRW_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
+	DeviceIoControl(driverHandle, VRW_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
 }
 
 template <typename T>
-T communication::read(u64 address) {
+T Communication::read(u64 address) {
 	T temp = {};
 
 	_PRW arguments = {};
@@ -111,23 +82,23 @@ T communication::read(u64 address) {
 	arguments.address = reinterpret_cast<void*>(address);
 	arguments.buffer = &temp;
 	arguments.size = sizeof(T);
-	arguments.process_id = process_id;
+	arguments.processId = processId;
 	arguments.Type = false;
 
-	DeviceIoControl(driver_handle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+	DeviceIoControl(driverHandle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
 
 	return temp;
 }
 
 template <typename T>
-void communication::write(u64 address, T& value) {
+void Communication::write(u64 address, T& value) {
 	_PRW arguments = {};
 	arguments.security_code = SECURITY_CODE;
 	arguments.address = reinterpret_cast<void*>(address);
 	arguments.buffer = (void*)&value;
 	arguments.size = sizeof(T);
-	arguments.process_id = process_id;
+	arguments.processId = processId;
 	arguments.Type = true;
 
-	DeviceIoControl(driver_handle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+	DeviceIoControl(driverHandle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
 }
