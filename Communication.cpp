f757816d0cb874c47b908138c39cabc0053be02e
ObamaRuntz <150731631+obamaruntz@users.noreply.github.com>
2025-07-11 20:20:10 +0100

    Update Communication.cpp

--- Communication.cpp
+++ Communication.cpp
@@ -1,91 +1,82 @@
-#include "Communication.h"
+#include <driver/communication.h>
 
-Communication::Communication() {
-    driverHandle = CreateFileA("\\\\.\\RickOwens00", GENERIC_READ | GENERIC_WRITE,
+communication::communication() {
+    driver_handle = CreateFileA("\\\\.\\RickOwens00", GENERIC_READ | GENERIC_WRITE,
         FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 }
 
-Communication::~Communication() {
-    if (driverHandle != INVALID_HANDLE_VALUE) {
-        CloseHandle(driverHandle);
+communication::~communication() {
+    if (driver_handle != INVALID_HANDLE_VALUE) {
+        CloseHandle(driver_handle);
     }
 }
 
-bool Communication::isConnected() {
-    return (driverHandle != INVALID_HANDLE_VALUE);
+bool communication::is_connected() {
+    return (driver_handle != INVALID_HANDLE_VALUE);
 }
 
-bool Communication::virtualAttach(i32 processId) {
+bool communication::v_attach(i32 process_id) {
     _VRW arguments;
-    arguments.processHandle = reinterpret_cast<HANDLE>(processId);
+    arguments.process_handle = reinterpret_cast<HANDLE>(process_id);
 
-    return DeviceIoControl(driverHandle, VRW_ATTACH_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
+    return DeviceIoControl(driver_handle, VRW_ATTACH_CODE, &arguments, sizeof(arguments), &arguments, sizeof(arguments), nullptr, nullptr);
 }
 
-std::string Communication::readString(u64 address) {
-    i32 stringLength = read<i32>(address + 0x18);
+std::string communication::readstr(u64 address) {
+    i32 StrLength = read<i32>(address + 0x18);
 
-    if (stringLength >= 16) {
+    if (StrLength >= 16) {
         address = read<u64>(address);
     }
 
-    std::vector<i8> buffer(256);
+    std::vector<i8> Buffer(256);
 
     _PRW arguments = {};
-    arguments.securityCode = SECURITY_CODE;
+    arguments.security_code = SECURITY_CODE;
     arguments.address = reinterpret_cast<void*>(address);
-    arguments.buffer = buffer.data();
-    arguments.size = buffer.size();
-    arguments.processId = processId;
-    arguments.type = false;
+    arguments.buffer = Buffer.data();
+    arguments.size = Buffer.size();
+    arguments.process_id = process_id;
+    arguments.Type = false;
 
-    DeviceIoControl(driverHandle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+    DeviceIoControl(driver_handle, PRW_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
 
-    return std::string(buffer.data());
+    return std::string(Buffer.data());
 }
 
-i32 Communication::findProcess(const i8* processName) {
-    PROCESSENTRY32 processEntry = {};
-    processEntry.dwSize = sizeof(PROCESSENTRY32);
+i32 communication::find_process(const i8* process_name) {
+    PROCESSENTRY32 process_entry = {};
+    process_entry.dwSize = sizeof(PROCESSENTRY32);
 
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (snapshot == INVALID_HANDLE_VALUE) {
         return 0;
     }
 
-    if (Process32First(snapshot, &processEntry)) {
+    if (Process32First(snapshot, &process_entry)) {
         do {
-            if (_stricmp(processEntry.szExeFile, processName) == 0) {
-                processId = processEntry.th32ProcessID;
+            if (_stricmp(process_entry.szExeFile, process_name) == 0) {
+                process_id = process_entry.th32ProcessID;
                 break;
             }
-        } while (Process32Next(snapshot, &processEntry));
+        } while (Process32Next(snapshot, &process_entry));
     }
 
     CloseHandle(snapshot);
 
-    return processId;
+    return process_id;
 }
 
-u64 Communication::findImage() {
+u64 communication::find_image() {
+    u64 image_address = 0;
     _BA arguments = {};
-    arguments.securityCode = SECURITY_CODE;
-    arguments.processId = processId;
-    arguments.address = &imageAddress;
+    arguments.security_code = SECURITY_CODE;
+    arguments.process_id = process_id;
+    arguments.address = &image_address;
 
-    DeviceIoControl(driverHandle, BA_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+    DeviceIoControl(driver_handle, BA_CODE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
 
-    return imageAddress;
-}
+    communication::image_address = image_address;
 
-HANDLE Communication::getDriverHandle() {
-    return driverHandle;
-}
-
-i32 Communication::getProcessId() {
-    return processId;
-}
-
-u64 Communication::getImageAddress() {
-    return imageAddress;
+    return image_address;
 }
